3bc3362272dae78686e45d7aa57214b7628989e1
Kinga <madking55@gmail.com>
2019-06-17 06:53:02 +0000

    Complete Variables

---
+++ Variables/age.rb
@@ -0,0 +1,5 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "In 10 years you will be #{age + 10} years old"
+puts "In 15 years you will be #{age + 15} years old"
+puts "In 20 years you will be #{age + 20} years old"
---
+++ Variables/name.rb
@@ -0,0 +1,7 @@
+puts "What is your first name?"
+firts_name = gets.chomp
+puts "What is your last name"
+last_name = gets.chomp
+puts "Welcome #{firts_name} #{last_name}!"
+
+5.times {puts last_name}
